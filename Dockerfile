8e5d5d9bf992435d781cf01ee166a84bb96185cc
jeffdvincent <jeffdvincent@penguin.lxd>
2021-12-08 21:30:45 -0800

    Seems to be working

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,12 @@
 FROM python:3.10.0-alpine3.14
-RUN apk update
-RUN apk add --update \
+RUN apk update && \
+    apk add --update \
 	ansible \
 	bash \
 	curl \
     openssh \
-    sshpass
-RUN apk add --upgrade openconnect
-RUN python3 -m pip install requests
+    sshpass && \
+    apk add --upgrade openconnect && \
+    python3 -m pip install requests
 COPY . .
-RUN python3 set_env.py
 ENTRYPOINT ["./run.sh"]
--- create_vm.py
+++ create_vm.py
@@ -1,11 +1,12 @@
+import argparse
 import json
 import os
 import requests
 
-class SpinUpOrkaVM:
+class SpinUpOrkaVM():
+
 	def __init__(self):
 		self.token = None
-		self.vm_name = os.environ['ANSIBLE_NAME_CONTAINS']
 		self.orka_address = 'http://10.221.188.100'
 		self.orka_user = os.environ['ORKA_USER']
 		self.orka_pass = os.environ['ORKA_PASS']
@@ -15,29 +16,29 @@ class SpinUpOrkaVM:
 
 	def get_auth_token(self):
 		data = {'email': self.orka_user, 'password': self.orka_pass}
-		result = requests.post(self.orka_address+'/token', data=data)
+		result = requests.post(f"{self.orka_address}/token", data=data)
 		content = json.loads(result._content.decode('utf-8'))
 		self.token = content['token']
 
-	def create_vm_config(self):
+	def create_vm_config(self, vm_name):
 		orka_address = f"{self.orka_address}/resources/vm/create"
 		headers = {
 			'Content-Type': 'application/json', 
 			'Authorization': f"Bearer {self.token}"
 		}
 		data = {
-			'orka_vm_name': self.vm_name,
+			'orka_vm_name': vm_name,
 			'orka_base_image': self.orka_base_image,
-			'orka_image': self.vm_name,
+			'orka_image': vm_name,
 			'orka_cpu_core': int(self.core_count),
 			'vcpu_count': int(self.vcpu_count)
 		}
 		requests.post(orka_address, data=json.dumps(data), headers=headers)
         
-	def deploy_vm_config(self):
+	def deploy_vm_config(self, vm_name):
 		orka_address = f"{self.orka_address}/resources/vm/deploy"
 		headers = {'Content-Type': 'application/json', 'Authorization': f"Bearer {self.token}"}
-		data =  {'orka_vm_name': self.vm_name}
+		data =  {'orka_vm_name': vm_name}
 		result = requests.post(orka_address, data=json.dumps(data), headers=headers)
 		content = json.loads(result._content.decode('utf-8'))
 		self.vm_ip = content['ip']
@@ -52,12 +53,20 @@ class SpinUpOrkaVM:
 		requests.delete(url, headers=headers)
 
 
-def main(spin_up):
+def parse_args():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--vm_name', dest='vm_name', action='store')
+	return parser.parse_args()
+
+
+def main(args, spin_up):
 	spin_up.get_auth_token()
-	spin_up.create_vm_config()
-	spin_up.deploy_vm_config()
-	spin_up.revoke_orka_auth_token()
+	spin_up.create_vm_config(args.vm_name)
+	spin_up.deploy_vm_config(args.vm_name)
+	# spin_up.revoke_orka_auth_token()
 	
+
 if __name__ == '__main__':
+	args = parse_args()
 	spin_up = SpinUpOrkaVM()
-	main(spin_up)
+	main(args, spin_up)
